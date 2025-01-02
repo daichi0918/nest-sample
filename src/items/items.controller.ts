fb49dfaa1cf300e42a2f76ad289dd12c2add1415
daichi0918 <kayasugadaichi@icloud.com>
2025-01-02 13:51:42 +0900

    finish:section3,バリデーションと例外処理

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -4,11 +4,13 @@ import {
   Delete,
   Get,
   Param,
+  ParseUUIDPipe,
   Post,
   Put,
 } from '@nestjs/common';
 import { ItemsService } from './items.service';
 import { Item } from './items.model';
+import { CreateItemDto } from './dto/create-item.dto';
 
 @Controller('items')
 export class ItemsController {
@@ -19,34 +21,28 @@ export class ItemsController {
   }
 
   @Get(':id')
-  findById(@Param('id') id: string): Item {
+  findById(@Param('id', ParseUUIDPipe) id: string): Item {
     return this.itemsService.findById(id);
   }
 
   @Post()
   create(
-    @Body('id') id: string,
-    @Body('name') name: string,
-    @Body('price') price: number,
-    @Body('price') description: string,
+    // @Body('id') id: string,
+    // @Body('name') name: string,
+    // @Body('price') price: number,
+    // @Body('price') description: string,
+    @Body() createItemDto: CreateItemDto,
   ): Item {
-    const item: Item = {
-      id,
-      name,
-      price,
-      description,
-      status: 'ON_SALE',
-    };
-    return this.itemsService.create(item);
+    return this.itemsService.create(createItemDto);
   }
 
   @Put(':id')
-  updateStatus(@Param('id') id: string) {
+  updateStatus(@Param('id', ParseUUIDPipe) id: string) {
     return this.itemsService.updateStatus(id);
   }
 
   @Delete(':id')
-  delete(@Param('id') id: string) {
+  delete(@Param('id', ParseUUIDPipe) id: string) {
     this.itemsService.delete(id);
   }
 }
