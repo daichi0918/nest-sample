8cd80b7f8fcd72101850f3df3f815084f56eb199
daichi0918 <kayasugadaichi@icloud.com>
2025-01-06 13:47:30 +0900

    finish:chapter46

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,5 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
-  console.log('abc');
 }
 bootstrap();
